4f8de427489cac34ebe6a6e92d030c50dcd412b0
yuji05225818@gmail.com <yuji05225818@gmail.com>
2017-04-21 21:10:21 +0900

    コンタクトRspec修正2

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,19 +4,18 @@ describe Contact do
 
   it "nameが空欄ならエラー" do
     contact = Contact.new(name: '', email: 'yuji0000@test.com', content: '問い合わせです')
-    expect(contact).not to eq_valid
+    expect(contact).not_to be_valid
   end
 
   it "emailが空欄ならエラー" do
     contact = Contact.new(name: '秦野', email: '', content: '問い合わせです')
-    expect(contact).not to eq_valid
+    expect(contact).not_to be_valid
   end
 
   it "問い合わせが空欄ならエラー" do
     contact = Contact.new(name: '秦野', email: 'yuji0000@test.com', content: '')
-    expect(contact).not to eq_valid
+    expect(contact).not_to be_valid
   end
-
 
   it "全てフォーム欄を満たしていると成功" do
     contact = Contact.new(name: '秦野', email: 'yuji0000@test.com', content: '問い合わせです')
