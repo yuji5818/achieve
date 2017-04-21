70e33df57a6956d9c3b4a91028349d878a64a3c8
yuji05225818@gmail.com <yuji05225818@gmail.com>
2017-04-21 20:47:10 +0900

    コンタクトRspec修正

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,15 +2,24 @@ require 'rails_helper'
 
 describe Contact do
 
-  # タイトルがあれば有効な状態であること
-  it "is valid with name and email" do
-    contact = Contact.new(name: '', email: '', content: '問い合わせです')
-    expect(contact).to be_valid
+  it "nameが空欄ならエラー" do
+    contact = Contact.new(name: '', email: 'yuji0000@test.com', content: '問い合わせです')
+    expect(contact).not to eq_valid
   end
 
-  #タイトルがなければ無効であること
-  it "is invalid without a name and an email" do
-    contact = Contact.new
-    expect(contact).not_to be_valid
+  it "emailが空欄ならエラー" do
+    contact = Contact.new(name: '秦野', email: '', content: '問い合わせです')
+    expect(contact).not to eq_valid
+  end
+
+  it "問い合わせが空欄ならエラー" do
+    contact = Contact.new(name: '秦野', email: 'yuji0000@test.com', content: '')
+    expect(contact).not to eq_valid
+  end
+
+
+  it "全てフォーム欄を満たしていると成功" do
+    contact = Contact.new(name: '秦野', email: 'yuji0000@test.com', content: '問い合わせです')
+    expect(contact). to eq_valid
   end
 end
